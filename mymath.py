655ef4b587264e015731c5867a684cd20a61daa9
Olav Vahtras <vahtras@kth.se>
2017-10-16 14:17:13 +0200

    Cleanup

--- mymath.py
+++ mymath.py
@@ -4,14 +4,6 @@ def mymax(x, y):
     else:
         return y
 
-i = 1
-j = 2
-#print("The maximum of", i, "and", j, "is",  mymax(i, j) )
-#print("The maximum of %d and %d is %d" % (i, j, mymax(i, j)))
-#print("The maximum of {} and {} is {}".format(i, j, mymax(i, j)))
-#print(f"The maximum of {i} and {j} is {mymax(i, j)}")
-
-
 def listmax(l):
     lmax = 0
     for i in l:
@@ -20,9 +12,6 @@ def listmax(l):
     return lmax
 
 
-mylist = [1, 2, 3]
-
-print(__name__)
-
 if __name__ == "__main__":
+    mylist = [1, 2, 3]
     print(listmax(mylist))
